a53d416226800470c943de83215b68e2d49edafa
vahid <vahid@DESKTOP-5L31PEO>
2017-11-14 07:53:08 +0330

    SetupProject

---
+++ Taxi-Pay.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Taxi_Pay.Infrastructure.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private Taxi_PayDbContext db = null;
+        private DbSet<T> table = null;
+        public GenericRepository()
+        {
+            this.db = new Taxi_PayDbContext();
+            table = db.Set<T>();
+        }
+        public GenericRepository(Taxi_PayDbContext db)
+        {
+            this.db = db;
+            table = db.Set<T>();
+        }
+
+        public IEnumerable<T> SelectAll()
+        {
+            return table.ToList();
+        }
+
+        public IQueryable<T> Table()
+        {
+            return table;
+        }
+
+        public IQueryable<T> TableUnTrack()
+        {
+            return table.AsNoTracking();
+        }
+
+        public T SelectByID(object id)
+        {
+            return table.Find(id);
+        }
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            db.Entry(obj).State = EntityState.Modified;
+        }
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Taxi-Pay.Infrastructure/Taxi-PayDbContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.Entity;
+
+namespace Taxi_Pay.Infrastructure
+{
+  public  class Taxi_PayDbContext:DbContext
+    {
+    }
+}
